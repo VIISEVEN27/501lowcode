7db51cd8c79073aeecc05ec924551ae4ab8d0bbb
VII <1035695237@qq.com>
2022-08-06 17:42:26 +0800

    chore: 修改代理ip，访问远程服务器的接口

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000/",
+        target: "http://139.224.220.27:3000/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
